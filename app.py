e6d728fa44c040a13bcbd2a6b1c3bf94302d9ab7
rushank7 <rushank.2008@gmail.com>
2022-09-16 13:04:06 +0200

    this is final commit with docker

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def predict_api():
 @app.route("/predict",methods=["Post"])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input = scalar.transform(np.array(list(data).reshape(1,-1)))
+    final_input = scalar.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output = regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="the house price prediction is {}".format(output))
